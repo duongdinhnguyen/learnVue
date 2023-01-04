f30b80dde6f4f1deda0bfeac2cd41a5d14571b3a
duonqdinhnquyen <nguyen.dinhduong@jvb-corp.com>
2023-01-04 11:04:01 +0700

    build project learn vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <header>
+    <button @click="count++">{{ count }}</button>
+  </header>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        count: 0
+      }
+    }
+  }
+</script>
