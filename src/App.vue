455e80ee23ea1cc0287229a11614d6ebc98941cb
duonqdinhnquyen <nguyen.dinhduong@jvb-corp.com>
2023-01-04 13:38:09 +0700

    diẻctive

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 
 <template>
   <header>
-    <button @click="count++">{{ count }}</button>
+    <form @submit.prevent="onSubmit">
+      <button v-on:click="increment">{{ count }}</button>
+      <span v-html="rawHtml"></span>
+      <span v-bind="elementAttribute">v-bind</span>
+      <button type="submit">Submit</button>
+    </form>
   </header>
 </template>
 
@@ -9,8 +14,26 @@
   export default {
     data() {
       return {
-        count: 0
+        count: 0,
+        title: "Đây là title",
+        rawHtml: `<span style="color: red">This should be red.</span>`,
+        elementAttribute: {
+          data: 1,
+          title: 'title Attribute'
+        }
       }
+    },
+    methods: {
+      increment() {
+        this.count ++
+      },
+      onSubmit() {
+        console.log(12222)
+      }
+
+    },
+    mounted() {
+      console.log(12345)
     }
   }
 </script>
