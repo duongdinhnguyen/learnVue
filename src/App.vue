8f4ed68e9f603a68320ee12315e17efe4a0d7f97
duonqdinhnquyen <nguyen.dinhduong@jvb-corp.com>
2023-01-04 14:11:37 +0700

    state - setup

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,23 @@
+<script setup>
+import { reactive} from "vue";
+    const state = reactive({ count: 0 })
+    function onSubmit() {
+      console.log(state.count)
+    }
+
+    function increment() {
+      state.count ++
+    }
+    
+</script>
 
 <template>
   <header>
     <form @submit.prevent="onSubmit">
-      <button v-on:click="increment">{{ count }}</button>
-      <span v-html="rawHtml"></span>
-      <span v-bind="elementAttribute">v-bind</span>
+      <button v-on:click="increment">{{ state.count }}</button>
+      <br>
       <button type="submit">Submit</button>
     </form>
   </header>
 </template>
 
-<script>
-  export default {
-    data() {
-      return {
-        count: 0,
-        title: "Đây là title",
-        rawHtml: `<span style="color: red">This should be red.</span>`,
-        elementAttribute: {
-          data: 1,
-          title: 'title Attribute'
-        }
-      }
-    },
-    methods: {
-      increment() {
-        this.count ++
-      },
-      onSubmit() {
-        console.log(12222)
-      }
-
-    },
-    mounted() {
-      console.log(12345)
-    }
-  }
-</script>
