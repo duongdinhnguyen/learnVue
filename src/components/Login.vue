a932236271851401a2b01fb8372be26ab98f9815
duonqdinhnquyen <nguyen.dinhduong@jvb-corp.com>
2023-01-31 09:22:34 +0700

    add API login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,18 +1,48 @@
+<script lang="ts" setup>
+    import { reactive } from "vue";
+    import axios from "axios";
+
+    const formData = reactive({
+        email: '',
+        password: ''
+    })
+
+    const handleSubmitLogin = () => {
+        axios({
+            method: "post",
+            url: "http://127.0.0.1:8000/api/auth/login",
+            data: formData,
+            headers: { "Content-Type": "multipart/form-data" },
+            })
+        .then(function (response) {
+            //handle success
+            console.log(response.data);
+        })
+        .catch(function (response) {
+            //handle error
+            console.log(response);
+        });
+    }
+</script>
 <template>
     <div>
-        <h2>Register</h2>
+        <h2>Login</h2>
     </div>
-    <div>
+    <form action="" @submit.prevent="handleSubmitLogin">
+        <div>
         <div>
             <label for="email">Email</label>
-            <input id="email" type="text">
+            <input id="email" type="text" v-model="formData.email">
         </div>
-
         <div>
             <label for="password">Password</label>
-            <input id="password" type="text">
+            <input id="password" type="text" v-model="formData.password">
+        </div>
+        <div>
+            <button type="submit">Login</button>
         </div>
     </div>
+    </form>
 </template>
 
 <style>
